b497e4d09cc4ec9dc049e03798ece70339ae16de
Piotr Badełek <piotrbadelek@protonmail.com>
2021-02-15 21:19:24 +0100

    usunięto sekretne API tzn co

--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,4 @@
 <?php
 $router->get('', 'PagesController@home');
 $router->post('results', 'PagesController@gameover');
-$router->get('kukanq', 'PagesController@api');
 //$router->post('users', 'UsersController@store'); for POSTs
